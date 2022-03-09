ff37e8eb66b24179f4a22091107c45b743c9afa8
Mohammed <you@example.com>
2022-03-09 15:47:31 +0200

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Bmisc();
+     return const MaterialApp(
+      
+      home: Bmisc(),
+    );
   }
 }
