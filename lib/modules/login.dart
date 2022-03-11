ea93bc37de364f2bdfbdce34b7e0cb4c1f9f1410
Mohammed <you@example.com>
2022-03-11 03:17:35 +0200

    commi

--- lib/modules/login.dart
+++ lib/modules/login.dart
@@ -1,9 +1,20 @@
 import 'package:bmi_calc2/shared/components/components.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool tap = true;
   var emailController = TextEditingController();
+
   var passwordController = TextEditingController();
+
+  var passwordController1 = TextEditingController();
+
+  var formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -13,104 +24,133 @@ class LoginScreen extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Login',
-                  style: TextStyle(
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(
-                  height: 40.0,
-                ),
-                TextFormField(
-                  controller: emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  onFieldSubmitted: (String value) {
-                    print(value);
-                  },
-                  onChanged: (String value) {
-                    print(value);
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Email Address',
-                    prefixIcon: Icon(
-                      Icons.email,
+            child: Form(
+              key: formkey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
                     ),
-                    border: OutlineInputBorder(),
                   ),
-                ),
-                SizedBox(
-                  height: 15.0,
-                ),
-                TextFormField(
-                  controller: passwordController,
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  onFieldSubmitted: (String value) {
-                    print(value);
-                  },
-                  onChanged: (String value) {
-                    print(value);
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    prefixIcon: Icon(
-                      Icons.lock,
-                    ),
-                    suffixIcon: Icon(
-                      Icons.remove_red_eye,
-                    ),
-                    border: OutlineInputBorder(),
+                  SizedBox(
+                    height: 40.0,
                   ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Container(
-                  width: double.infinity,
-                  color: Colors.blue,
-                  child: MaterialButton(
-                    onPressed: () {
-                      print(emailController.text);
-                      print(passwordController.text);
+                  TextFormField(
+                    validator:(value) {
+                      if (value!.isEmpty){
+                       return "Gmail must not be empty!";
+                      }return null;
+                      
+                    }, 
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    onFieldSubmitted: (String value) {
+                      print(value);
                     },
-                    child: Text(
-                      'LOGIN',
-                      style: TextStyle(
-                        color: Colors.white,
+                    onChanged: (String value) {
+                      print(value);
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Email Address',
+                      prefixIcon: Icon(
+                        Icons.email,
                       ),
+                      border: OutlineInputBorder(),
                     ),
                   ),
-                ),
-                def_button( title: "helloooo" , func: (){
-                  print("hello boi");
-                }),
-                def_button( title: "iam here" , func: (){
-                  // ignore: avoid_print
-                  print("hello boi");
-                }),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Don\'t have an account?',
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Register Now',
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  TextFormField(
+                    validator:(value) {
+                      if (value!.isEmpty){
+                       return "Password must not be empty!";
+                      }return null;
+                      
+                    }, 
+                    controller: passwordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: tap,
+                    onFieldSubmitted: (String value) {
+                      print(value);
+                    },
+                    onChanged: (String value) {
+                      print(value);
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      prefixIcon: Icon(
+                        Icons.lock,
                       ),
+                      suffixIcon: GestureDetector(
+                        onTap: (){
+                          setState(() {
+                            tap = !tap;
+                            
+                          });
+                        },
+                        child: Icon(
+                          tap ? Icons.visibility: Icons.visibility_off
+                        ),
+                      ),
+                      border: OutlineInputBorder(),
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  def_text_form(Controller: passwordController1 ,Border: UnderlineInputBorder() , 
+                  label: "lol" , on_edit: (){print ("lol");} , on_sub: (){print("XD");} , 
+                  preicon: Icons.sailing , sufficon: Icons.local_airport , val_str: "fucck u " , Obsecure_text: false)
+                  ,
+                   SizedBox(
+                    height: 20.0,
+                  ),
+                  def_button(bor_radius: 20,
+                    func: () {
+                      if (formkey.currentState!.validate()){
+                      print(emailController.text);
+                      print(passwordController.text);}
+                    },
+                    color: Colors.blue,
+                    upper: true,
+                    title: "Login"
+                  ),
+                   SizedBox(
+                    height: 10.0,
+                  ),
+                   def_button(
+                     bor_radius: 20,
+                    func: () {
+                     
+                    },
+                    color: Colors.green,
+                    upper: true,
+                    title: "Register"
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Don\'t have an account?',
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Register Now',
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -7,18 +7,62 @@ Widget def_button({
   double height = 40,
   bool upper = true,
   required Function func,
+  double bor_radius = 0,
 }) =>
     Container(
       height: height,
       width: width,
-      color: color,
+      decoration: BoxDecoration(
+          color: color, borderRadius: BorderRadius.circular(bor_radius)),
       child: MaterialButton(
-        onPressed: (){},
+        onPressed: () {
+          func();
+        },
         child: Text(
-          upper ? title.toUpperCase() : title , 
+          upper ? title.toUpperCase() : title,
           style: TextStyle(
             color: Colors.white,
           ),
         ),
+      ),
+    );
+
+Widget def_text_form(
+        {required var Controller,
+        Function? on_sub,
+        Function? on_edit,
+        String label = "label" , 
+        IconData? preicon ,
+        IconData ? sufficon  ,
+        InputBorder Border = const OutlineInputBorder(),
+        required String ? val_str,
+        bool Obsecure_text = false, 
+
+        }) =>
+    TextFormField(
+      validator: (value) {
+        if (value!.isEmpty) {
+          return val_str;
+        }
+        return null;
+      },
+      controller: Controller,
+      keyboardType: TextInputType.visiblePassword,
+      obscureText: Obsecure_text,
+      onFieldSubmitted: (String value) {
+        on_sub!();
+      },
+      onChanged: (String value) {
+        on_edit!();
+      },
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: Icon(
+          preicon,
+        ),
+        suffixIcon: Icon(
+          sufficon,
+        ),
+        border: OutlineInputBorder(),
       ),
     );
