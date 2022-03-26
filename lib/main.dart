285e1e73cb44554871692a65aa5f7536e5dd4d97
Mohammed <you@example.com>
2022-03-26 23:21:05 +0200

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:bloc/bloc.dart';
 import 'package:bmi_calc2/layout/Franc_layout.dart';
 import 'package:bmi_calc2/layout/NewLayout.dart';
@@ -9,19 +11,18 @@ import 'package:bmi_calc2/modules/counter/counter.dart';
 import 'package:bmi_calc2/modules/login.dart';
 import 'package:bmi_calc2/shared/Block_obs.dart';
 import 'package:bmi_calc2/shared/Cubit/cubit.dart';
+import 'package:bmi_calc2/shared/network/remote/DIo_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
   BlocOverrides.runZoned(
     () {
-    runApp(const MyApp());
+      runApp(const MyApp());
     },
     blocObserver: MyBlocObserver(),
   );
-   
-  
-  
+  DioHelper.init();
 }
 
 class MyApp extends StatelessWidget {
@@ -30,21 +31,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    
-     return MaterialApp(
-       debugShowCheckedModeBanner: false,
-       theme: ThemeData(
-         appBarTheme: AppBarTheme(
-           backwardsCompatibility: false,
-           systemOverlayStyle: SystemUiOverlayStyle(statusBarColor: Colors.red 
-           , statusBarIconBrightness: Brightness.light),
-           color: Colors.red , 
-           titleTextStyle: TextStyle(
-             fontSize: 30 , 
-           )
-         ) , 
-       ),
-      
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        floatingActionButtonTheme:
+            FloatingActionButtonThemeData(backgroundColor: Colors.deepOrange),
+        appBarTheme: AppBarTheme(
+            backwardsCompatibility: false,
+            systemOverlayStyle: SystemUiOverlayStyle(
+                statusBarColor: Colors.red,
+                statusBarIconBrightness: Brightness.light),
+            color: Colors.red,
+            titleTextStyle: TextStyle(
+              fontSize: 30,
+            )),
+      ),
       home: NewLayout(),
     );
   }
--- lib/shared/Cubit/NewsCubit.dart
+++ lib/shared/Cubit/NewsCubit.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:bloc/bloc.dart';
 import 'package:bmi_calc2/modules/Newsapp/Business.dart';
 import 'package:bmi_calc2/modules/Newsapp/Sports.dart';
@@ -6,29 +8,40 @@ import 'package:bmi_calc2/shared/Cubit/NewsStates.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../modules/Newsapp/Setting.dart';
+import '../network/remote/DIo_helper.dart';
 
-class NewsCubit extends Cubit<NewsStates>{
+class NewsCubit extends Cubit<NewsStates> {
   NewsCubit() : super(InitNewsStated());
-  static NewsCubit get (context) => BlocProvider.of(context);
+  static NewsCubit get(context) => BlocProvider.of(context);
   int Index = 0;
-  void ChangeItem (int ind)
-  {
+  void ChangeItem(int ind) {
     Index = ind;
     emit(ChangeSelectedNavItem());
   }
-  List<Widget> screens = 
-  [
-  Typr1() , 
-  Sports() , 
-  Business() , 
-  Setting(),
-  ];
-   List<String> Tytles = 
-  [
-  "Fashion" , 
-  "Sports", 
-  "Business", 
-  "Setting"
-  ];
 
+  List<Widget> screens = [
+    Typr1(),
+    Sports(),
+    Business(),
+    Setting(),
+  ];
+  List<String> Tytles = ["Fashion", "Sports",
+   "Business", "Setting"];
+  List<dynamic> BusinessNews = [];
+  void GetBusinNews() {
+    DioHelper.getData(
+      path: 'v2/top-headlines',
+      query: {
+        'country': 'eg',
+        'category': 'business',
+        'apiKey': '06030ba9455c45baabd0bfeaaf2d86de'
+      },
+    ).then((value) {
+      
+      BusinessNews = value.data['articles'];
+      print(BusinessNews[0]['source']["id"]);
+    }).catchError((error) {
+      print(error.toString());
+    });
+  }
 }
